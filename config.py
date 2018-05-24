36d247a1d40d437e10435f7570246c0f2f8a3e55
Hannes <ga74bux@mytum.de>
2018-05-24 16:10:55 +0200

    config trial

--- config.py
+++ config.py
@@ -1,22 +1,21 @@
 class Config:
     
     # directories
-    summary_dir = "summary/ResNet50"
+    summary_dir = "summary/LeNet"
     checkpoint_dir = "checkpoints/c1"
     
     num_classes = 4
     
     train_on_subset = True
-    subset_size = 200
+    subset_size = 40
     
     
-    model_type = 'ResNet50'
+    model_type = 'LeNet'
     available_model_types = {'LeNet', 'ResNet50'}
     learning_rate = 1e-4
     train_val_split = 0.8
     batch_size = 8
-    num_epochs = 100
+    num_epochs = 20
     
     max_to_keep = 1
     save_models = False
-    
