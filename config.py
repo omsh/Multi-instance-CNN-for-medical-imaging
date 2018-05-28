f66e08edc98956ac4fef75359ea7b86bdeb82c63
Shouman <omar.shouman@gmail.com>
2018-05-29 00:37:59 +0200

    config file parameters and run file edits for patch extraction and training (pending but working)

--- config.py
+++ config.py
@@ -1,22 +1,40 @@
 class Config:
     
     # directories
-    summary_dir = "summary/ResNet50"
+    summary_dir = "summary/RN50_patch"
     checkpoint_dir = "checkpoints/c1"
     
+    # hardware parameters
+    num_parallel_cores = 8
+    
+    # Dataset parameters
     num_classes = 4
     
+    # training on a subset of the data, for testing and prototyping
     train_on_subset = True
-    subset_size = 200
+    subset_size = 100
+    
+    # patch geneartion (currently on the fly, can be extended to save on the hard disk)
+    train_on_patches = True
+    patch_size = 224
+    
+    # patch sampling (not used yet)
+    n_patches_to_sample_per_image = 32
     
     
+    # Training parameters
+    train_val_split = 0.8
+    
+    # Model parameters
     model_type = 'ResNet50'
     available_model_types = {'LeNet', 'ResNet50'}
-    learning_rate = 1e-4
-    train_val_split = 0.8
-    batch_size = 8
-    num_epochs = 100
     
+    learning_rate = 1e-4
+    batch_size = 128
+    num_epochs = 20
+    
+    # Model saving parameters
     max_to_keep = 1
     save_models = False
+    
     
