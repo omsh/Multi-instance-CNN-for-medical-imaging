63d5666f57a35abc68d034c788ecb14259a4eddc
Omar Shouman <omar.shouman@gmail.com>
2018-05-31 02:11:54 +0200

    added pre-augmentation rotation angles in config.

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config:
     
     # directories
-    summary_dir = "summary/RN50_patch"
+    summary_dir = "summary/testing"
     checkpoint_dir = "checkpoints/c1"
     
     # hardware parameters
@@ -10,9 +10,15 @@ class Config:
     # Dataset parameters
     num_classes = 4
     
+    # Augmentation
+    
+    #  This is pre-augmentation random rotations for images (applied before patching)
+    # angles in degrees and rotation is counter-clock-wise
+    rotation_angles = [0, 90, 180, 270]
+    
     # training on a subset of the data, for testing and prototyping
     train_on_subset = True
-    subset_size = 100
+    subset_size = 40
     
     # patch geneartion (currently on the fly, can be extended to save on the hard disk)
     train_on_patches = True
@@ -21,9 +27,10 @@ class Config:
     # patch sampling (not used yet)
     n_patches_to_sample_per_image = 32
     
-    
     # Training parameters
     train_val_split = 0.8
+    
+    # Optimizer parameters
     
     # Model parameters
     model_type = 'ResNet50'
@@ -31,7 +38,7 @@ class Config:
     
     learning_rate = 1e-4
     batch_size = 128
-    num_epochs = 20
+    num_epochs = 2
     
     # Model saving parameters
     max_to_keep = 1
